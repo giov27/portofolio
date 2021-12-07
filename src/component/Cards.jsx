6d5ced9c80db830a8a69ec02aa843dfb0e59e631
giov27 <giovannimaendra@gmail.com>
2021-12-07 10:01:10 +0700

    Update project list

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -22,8 +22,20 @@ const Cards = () => {
               label='Web Dev'
               path="https://beasy.vercel.app/"
             />
+            <CardItem 
+              src="https://garudahack-elevatap.herokuapp.com/images/logo/LOGO_Elevatap.svg"
+              text='Elevatap is the prototype to support the small and middle bussiness to get their legality'
+              label='Web Dev'
+              path="https://garudahack-elevatap.herokuapp.com/"
+            />
           </ul>
           <ul className="cards__items">
+            <CardItem 
+              src="https://cdn.dribbble.com/users/5920881/screenshots/14088635/media/da11146df7c3cb056c8d8aa60333b935.gif"
+              text='Simple apps to show the list, add, update, and delete data of patient in hospital'
+              label='Web Dev'
+              path="https://github.com/ikhwanwan07/Frontend-Mamasakan"
+            />
             <CardItem 
               src="https://raw.githubusercontent.com/ikhwanwan07/Frontend-Mamasakan/master/img/logo.png"
               text='Mamasakan'
